b1f5cc1415194231bfad924b7a780b5b9a19ef36
= <=>
2022-12-08 05:46:54 +0300

    added a class to handle transacrtions

---
+++ src/Transaction.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace SsentezoWallet;
+
+/**
+ * This class represents a transction in ssentezo wallet.
+ * It has helpers to help you seemlessly handle transactiosn in ssentezo wallet
+ * 
+ */
+class Transaction
+{
+}
