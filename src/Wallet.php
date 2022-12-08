96e58f4f5675f58fb024c0f0130f09ca9f7a5e79
= <=>
2022-12-08 06:02:47 +0300

    Moved method to Wallet from Ssentezo wallet

--- src/Wallet.php
+++ src/Wallet.php
@@ -2,9 +2,24 @@
 
 namespace SsentezoWallet;
 
+use Exception;
+use Unirest\Request;
+
 /**
  * This is the class with the basic wallet operations
  */
 class Wallet
 {
+    protected function sendRequest()
+    {
+        try {
+            $headers = array('Content-Type: multipart/form-data');
+            $body = $this->payload;
+            Request::auth($this->username, $this->password);
+            $response = Request::post($this->endPoint, $headers, $body);
+        } catch (Exception $e) {
+            print_r($e);
+        }
+        return $response;
+    }
 }
