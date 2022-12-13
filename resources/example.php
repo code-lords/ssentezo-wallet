6b71943b1455012a0fc095cdd5a9d073690d8c92
= <=>
2022-12-13 03:37:36 +0300

    added more functionlity to wallet

--- resources/example.php
+++ resources/example.php
@@ -1,11 +1,12 @@
 <?php
 require_once "../vendor/autoload.php";
 
-use SsentezoWallet\SentezoWallet;
+use SsentezoWallet\SsentezoWallet;
 
 $username = "523d142a-725a-440e-95f4-3bb6c78889ec";
 $password = "3f4b7f66e33b853e7f49d4b2a556edaf";
-$wallet = new SentezoWallet($username, $password);
+$wallet = new SsentezoWallet($username, $password);
 // $wallet->setEnvironment("sandbox");
-$res = $wallet->withdraw("0771397135", 1000, "abcdef0ko", "Deposit to my account", "https://www.google.com");
-print_r($res->body);
+// $res = $wallet->withdraw("0771397135", 1000, "gabcdefl'ko", "Deposit to my account", "https://www.google.com");
+$res = $wallet->deposit('0756292975', 1000, 'sed', "NOne", "ggggggg");
+print_r($res);
