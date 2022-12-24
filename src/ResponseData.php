6b908f39cf06b4f9e0a1114358dc24d4468370d3
= <=>
2022-12-24 15:56:34 +0300

    added a new file for respobse data

---
+++ src/ResponseData.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace SsentezoWallet;
+
+use Exception;
+
+/**
+ * This represents the data part of the wallet response
+ */
+class ResponseData
+{
+
+
+    // "data" : {
+    //     "externalReference" : "2381", 
+    //     "request_id" : "b997c60c6f445185fcd9a3a595533734" , 
+    //     "transactionStatus" : "FAILED", 
+    //     "financialTransactionId" : ""  
+    // }
+
+    // "data" : {
+    //     "externalReference" : "2391", 
+    //     "request_id" : "b997c60c6f445185fcd9a3a595533734" , 
+    //     "transactionStatus" : "SUCCEEDED", 
+    //     "financialTransactionId" : "12282913328"  
+    // }
+
+    // "data" : {
+    //     "externalReference" : "2391", 
+    //     "request_id" : "b997c60c6f445185fcd9a3a595533734" , 
+    //     "transactionStatus" : "PENDING", 
+    // }
+    // "data" : {
+    //     "externalReference" : "2391", 
+    //     "request_id" : "b997c60c6f445185fcd9a3a595533734" , 
+    //     "transactionStatus" : "FAILED", 
+    // }
+}
