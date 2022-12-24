e03f4d4fca3e0a14fa62bc6d0668cb306d762189
= <=>
2022-12-24 15:59:17 +0300

    edited balance inquiry transaction

--- src/transactions/BalanceInquiryTransaction.php
+++ src/transactions/BalanceInquiryTransaction.php
@@ -1,3 +1,32 @@
 <?php
 
 namespace SsentezoWallet\Transactions;
+
+class BalanceInquiryTransaction extends Transaction
+{
+    /**
+     * @var float 
+     * The account balance
+     */
+    public $amount;
+
+    /**
+     * @var string
+     * The account balance formatted
+     */
+    public $formattedAmount;
+
+    /**
+     * @var string
+     * The currency of the account
+     */
+    public $currency;
+
+    public function __construct($response)
+    {
+        parent::__construct($response);
+        $this->amount = $response->data->amount;
+        $this->formattedAmount = $response->data->formatted;
+        $this->currency = $response->data->currency;
+    }
+}
