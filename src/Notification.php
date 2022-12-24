e140e9d7c6591d1aed9eda07145016cb08e37932
= <=>
2022-12-24 15:58:01 +0300

    edited notification class

---
+++ src/Notification.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace SsentezoWallet;
+
+class Notification
+{
+    /**
+     * @var string
+     */
+    public $message;
+    /**
+     * @var string
+     */
+    public $status;
+    /**
+     * @var string
+     */
+    public $errorCode;
+    /**
+     * @var array
+     */
+    public $data;
+    /**
+     * Response constructor.
+     * @param \Unirest\Response $res
+     */
+    public function __construct(\Unirest\Response $res)
+    {
+        $body = $res->body;
+        $this->errorCode = $body->errorCode;
+        $this->message = $body->message;
+        $this->status = $body->status;
+        $this->data = (array)$body->data;
+    }
+}
