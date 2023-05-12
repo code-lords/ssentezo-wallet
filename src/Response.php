8a44cd106b9ade463f6254a9c30ac5b178eb9c40
Daaki Benjamin <richarddaaki4@gmail.com>
2023-05-12 10:27:47 +0300

    quick fix (Status and error codes removed )

--- src/Response.php
+++ src/Response.php
@@ -26,15 +26,37 @@ class Response
     public $data;
 
     /**
+     * The external reference
+     */
+    public $externalReference;
+
+    /**
+     * The id of the request
+     */
+    public $request_id;
+
+    /**
+     * Transaction  status
+     */
+    public $transactionStatus;
+    /**
+     * Finacial transaction id
+     */
+    public $financialTransactionId;
+    /**
      * Response constructor.
      * @param \Unirest\Response $res
      */
     public function __construct(\Unirest\Response $res)
     {
         $body = $res->body;
-        $this->errorCode = $body->errorCode;
+        $this->errorCode = ""; //$body->errorCode;
         $this->message = $body->message;
-        $this->status = $body->status;
+        $this->status = ""; //$body->status;
         $this->data = (array)$body->data;
+        $this->externalReference = $this->data["externalReference"];
+        $this->request_id = $this->data["request_id"];
+        $this->transactionStatus = $this->data['transactionStatus'];
+        $this->financialTransactionId = $this->data['financialTransactionId'] ?? "";
     }
 }
